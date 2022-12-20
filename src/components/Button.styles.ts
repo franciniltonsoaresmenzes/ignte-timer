974d0279fecc71a00313895df3a65844d0d684ed
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-19 22:20:33 -0300

    definindo fonts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,8 +20,8 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     margin-right: 12px;
     border-radius: 8px;
 
-    background-color: ${props => props.theme.primary};
-    color: ${props => props.theme.secondary};
+    background-color: ${props => props.theme["green-500"]};
+    color: ${props => props.theme.white};
 /* 
     ${props => (
         css`background-color: ${buttonVariants[props.variant]};`
